ca2bde9a109f72bbe43c8d46ee70d8e208023e42
Mr.Dilli <121505595+dillu143@users.noreply.github.com>
2023-12-05 09:43:22 +0530

    Update code.tf

--- code.tf
+++ code.tf
@@ -85,10 +85,19 @@ resource "aws_security_group" "security" {
     }
 }
 
-#key pair
-resource "aws_key_pair" "kp" {
-  key_name   = "myKey"       # Create a my key
-  public_key = "ssh-rsa-key"
+
+###key-pair
+resource "aws_key_pair" "key_pair" {
+  key_name   = "key-pair"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+resource "local_file" "tf-key" {
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "key-pair"
 }
 
 #instance
